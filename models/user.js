0cc475f541f7eeed3cd625765212edc077e0bab2
Vivek Baruah <vivekbrh01@gmail.com>
2020-05-11 02:11:17 +0530

    minor changes

--- models/user.js
+++ models/user.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: /@/
-    },
-    age: Number,
-    sports: [String],
-
-}, 
-{timestamps: true}
+const userSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			match: /@/,
+		},
+		age: Number,
+		sports: [String],
+	},
+	{ timestamps: true }
 );
 
 // const User = mongoose.model('User', userSchema);
 // module.exports = User;
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
